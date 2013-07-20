0be5e1ea81fb3967c409e0da66171709d1711fc4
Luke <doclukemckee@gmail.com>
2013-07-20 14:44:21 +1200

    properties and submission from subreddit getter

--- bot/bot.py
+++ bot/bot.py
@@ -11,6 +11,7 @@ USERNAME    = ""
 
 class RedditBot(object):
     """
+    https://praw.readthedocs.org/en/latest/pages/writing_a_bot.html
     RedditBot: 
         Configurations
             tracks subreddits to check rules in
@@ -23,7 +24,44 @@ class RedditBot(object):
             check if URL is a comment on reddit
             login as user x
     """
+    # set unique and descriptive user_agent
+    __user_agent    = "reddit_bot_test v0.0 by /u/drhealsgood and /u/"
+    # create Reddit object
+    __reddit        = praw.Reddit(__user_agent)
 
-
-    def __init__(self,rules):
+    def __init__(self,rules,subreddits):
+        # log in
+        self.__reddit.login(USERNAME,getpass())
+        self.__rules        = rules
+        self.__subreds      = subreddits
+        # submission: comments
+        self.__submissions  = {}
         
+    @property
+    def submissions(self):
+        return self.__submissions
+    
+    @property
+    def rules(self):
+        return self.__rules
+    
+    def add_rule(self, rule):
+        self.__rules.append(rule)
+    
+    @property
+    def subreddits(self):
+        return self.__subreds
+    
+    @subreddits.setter
+    def subreddits(self,new_subreddits):
+        self.__subreddits   = new_subreddits
+    
+    def add_subreddit(self,subreddit):
+        self.__subreds.append(subreddit)
+        
+    def _get_submissions(self,subreddit,n):
+        """
+            returns top n submissions from
+            subreddit
+        """
+        return self.__reddit.get_subreddit(subreddit).get_top(limit=n)
